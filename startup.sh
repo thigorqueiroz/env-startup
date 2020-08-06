ccfc44ff101ce9691712bc465e527d802d6c0876
Thiago Queiroz <thigorqueiroz@gmail.com>
2020-08-06 17:58:34 -0300

    startup script

---
+++ startup.sh
@@ -0,0 +1,208 @@
+sudo apt-get update
+
+
+
+echo 'Do you want to install curl ?(y/n)'
+read curl_install
+if echo "$curl_install" | grep -iq "^y";then 
+sudo apt install curl -y
+else 
+ echo "Curl is not a choice"
+fi
+
+
+echo "Installing SDKMan"
+curl -s "https://get.sdkman.io" | bash 
+
+echo 'Do you want to install SCALA ?(y/n)'
+read scala_install
+if echo "$scala_install" | grep -iq "^y";then 
+sdk install sbt && sdk install scala
+echo "Scala installed by SDKMan"
+else 
+ echo "Scala is not a choice"
+fi
+
+echo 'Do you want to install GOLANG ?(y/n)'
+read golang_install
+if echo "$golang_install" | grep -iq "^y";then 
+curl -L "https://golang.org/dl/go1.14.7.linux-amd64.tar.gz" -o "go1.14.7.linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf go1.14.7.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+echo "golang INSTALLED!"
+else 
+ echo "golang is not a choice"
+fi
+
+echo 'installing git' 
+sudo apt install git -y
+
+echo "What is global name do you want to use in GIT user.name?"
+echo "For example: \"Thiago Queiroz\""
+read git_config_user_name
+git config --global user.name "$git_config_user_name"
+clear 
+
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"thigorqueiroz@gmail.com\""
+read git_config_user_email
+git config --global user.email $git_config_user_email
+clear
+
+echo "Can I set VIM as your default GIT editor for you? (y/n)"
+read git_core_editor_to_vim
+if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+	git config --global core.editor vim
+else
+	echo "Okay, no problem. :) Let's move on!"
+fi
+
+echo "Generating a SSH Key"
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+echo 'installing docker' 
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+sudo chmod 777 /var/run/docker.sock
+docker run hello-world
+
+echo 'installing docker-compose' 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo 'installing aws-cli' 
+sudo apt-get install awscli -y
+aws --version
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+sudo dpkg -i session-manager-plugin.deb
+session-manager-plugin --version
+
+echo 'installing fzf'
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+echo "installing microsoft teams"
+curl -L "https://go.microsoft.com/fwlink/p/?linkid=2112886&clcid=0x409&culture=en-us&country=us" -o "teams.deb"
+sudo dpkg -i  ./teams.deb 
+
+echo 'enabling workspaces for both screens' 
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+echo 'installing zsh'
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+
+echo 'installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
+
+export alias pbcopy='xclip -selection clipboard'
+export alias pbpaste='xclip -selection clipboard -o'
+source ~/.zshrc
+
+echo 'installing vim'
+sudo apt install vim -y
+clear
+
+echo 'installing code'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code -y # or code-insiders
+
+echo 'installing extensions'
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dracula-theme.theme-dracula
+code --install-extension esbenp.prettier-vscode
+code --install-extension foxundermoon.shell-format
+code --install-extension pmneo.tsimporter
+code --install-extension waderyan.gitblame
+code --install-extension yzhang.markdown-all-in-one
+
+echo 'installing spotify' 
+snap install spotify
+
+echo 'installing chrome' 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+
+source ~/.zshrc
+
+echo 'installing autosuggestions' 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+echo 'installing theme'
+sudo apt install fonts-firacode -y
+wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme 
+sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
+
+echo 'installing slack' 
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
+sudo apt install ./slack-desktop-*.deb -y
+
+echo 'installing terminator'
+sudo apt-get update
+sudo apt-get install terminator -y
+
+echo 'adding dracula theme' 
+cat <<EOF >  ~/.config/terminator/config
+[global_config]
+  title_transmit_bg_color = "#ad7fa8"
+[keybindings]
+  close_term = <Primary>w
+  close_window = <Primary>q
+  new_tab = <Primary>t
+  new_window = <Primary>i
+  paste = <Primary>v
+  split_horiz = <Primary>e
+  split_vert = <Primary>d
+go  switch_to_tab_1 = <Primary>1
+  switch_to_tab_10 = <Primary>0
+  switch_to_tab_2 = <Primary>2
+  switch_to_tab_3 = <Primary>3
+  switch_to_tab_4 = <Primary>4
+  switch_to_tab_5 = <Primary>5
+  switch_to_tab_6 = <Primary>6
+[layouts]
+  [[default]]
+    [[[child1]]]
+      parent = window0
+      type = Terminal
+    [[[window0]]]
+      parent = ""
+      type = Window
+[plugins]
+[profiles]
+  [[default]]
+    cursor_color = "#aaaaaa"
+EOF
+
+
+cat <<EOF >>  ~/.config/terminator/config
+[[Dracula]]
+    background_color = "#1e1f29"
+    background_darkness = 0.88
+    background_type = transparent
+    copy_on_selection = True
+    cursor_color = "#bbbbbb"
+    foreground_color = "#f8f8f2"
+    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
+    scrollback_infinite = True
+EOF
+
+sudo apt auto-remove
+
