e064514be6ba31c538c580f33414c2672da00049
Thiago Queiroz <thigorqueiroz@gmail.com>
2020-08-10 09:55:25 -0300

    updating aws-cli

--- startup.sh
+++ startup.sh
@@ -78,8 +78,10 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 echo 'installing aws-cli' 
-sudo apt-get install awscli -y
-aws --version
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
 session-manager-plugin --version
